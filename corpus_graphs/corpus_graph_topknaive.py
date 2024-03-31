55172755f70d32a15017aa4467d595fc861bc2c7
LachlanJDunn <lachlan_dunn6@outlook.com>
2024-03-31 12:50:03 +1000

    1

--- corpus_graphs/corpus_graph_topknaive.py
+++ corpus_graphs/corpus_graph_topknaive.py
@@ -30,7 +30,7 @@ logger = ir_datasets.log.easy()
 
 
 class CorpusGraph:
-  def neighbours(self, docid: Union[int, str], weights: bool = False) -> Union[np.array, List[str], Tuple[np.array, np.array], Tuple[List[str], np.array]]:
+  def neighbours(self, docid: Union[int, str]) -> Union[np.array, List[str], Tuple[np.array, np.array], Tuple[List[str], np.array]]:
     raise NotImplementedError()
 
   #
@@ -57,13 +57,11 @@ class CorpusGraph:
       raise FileExistsError(out_dir)
     out_dir.mkdir(parents=True)
     edges_path = out_dir/'edges.u32.np'
-    weights_path = out_dir/'weights.f16.np'
 
     doc_size = 8841823
-    ids = pd.DataFrame(data={'id': [-1 for i in range(doc_size)]})
+    id_list = []
     id_count = 0
-    count = 0
-    temp = 0
+    scored_count = 0
 
     # First step: We need a docno <-> index mapping for this to work. Do a pass over the iterator
     # to build a docno loookup file, while also writing the contents to a temporary file that we'll read
@@ -73,22 +71,25 @@ class CorpusGraph:
       with LZ4FrameFile(f'{dout}/docs.pkl.lz4', 'wb') as fout:
         for doc in logger.pbar(docs_it, miniters=1, smoothing=0, desc='first pass'):
           pickle.dump(doc, fout)  # write serialized docs to temp file
+          id_list.append(int(doc.doc_id))
+      ids = pd.DataFrame(data={'id': [-1 for i in range(doc_size)]}, index=id_list)
 
       # Perform retrieval in chunks for efficiency
-      with ir_datasets.util.finialized_file(str(edges_path), 'wb') as fe, ir_datasets.util.finialized_file(str(weights_path), 'wb') as fw, LZ4FrameFile(f'{dout}/docs.pkl.lz4', 'rb') as fin, Lookup.builder(out_dir/'docnos.npids') as docnos:
+      with ir_datasets.util.finialized_file(str(edges_path), 'wb') as fe, LZ4FrameFile(f'{dout}/docs.pkl.lz4', 'rb') as fin, Lookup.builder(out_dir/'docnos.npids') as docnos:
         for chunk in more_itertools.chunked(logger.pbar(range(doc_size), miniters=1, smoothing=0, desc='searching', total=doc_size), batch_size):
           chunk = [pickle.load(fin) for _ in chunk]  # creates list of docs
           chunk_df = pd.DataFrame(chunk).rename(
               columns={'docno': 'qid', 'text': 'query'})
-          to_drop = ids.iloc[[int(i)-1 for i in chunk_df.qid.to_numpy()]]
+          to_drop = ids.loc[[int(i) for i in chunk_df.qid.to_numpy()]]
           to_drop = to_drop.loc[to_drop['id'] != -1]
           # remove already scored documents
-          chunk_df = chunk_df.drop([i % batch_size for i in to_drop.index])
-          temp += len(chunk_df.index)
+          chunk_df = chunk_df.drop(to_drop.index)
+          scored_count += len(chunk_df.index)
           res = retriever(chunk_df)  # result of retrieval of one chunk
           # mapping of qid to query/docno/docno/score/rank (as multiple queries in chunk)
           res_by_qid = dict(iter(res.groupby('qid')))
           for docno in chunk_df['qid']:  # loops through docnos in chunk
+            new_docs_count = 0
             # results for one query (ie. document)
             did_res = res_by_qid.get(docno)
             dids = []
@@ -102,37 +103,30 @@ class CorpusGraph:
                 for i in range(len(did_res.index)):
                   if append_doc == True and i == len(did_res.index) - 1:
                     docnos.add(docno)
-                    ids.loc[int(docno)-1, 'id'] = id_count
+                    new_docs_count += 1
+                    ids.loc[int(docno), 'id'] = id_count
                     dids.append(id_count)
                     id_count += 1
                   else:
-                    docno2 = did_res.iloc[i]['docno']
-                    if ids.iloc[int(docno2)-1]['id'] == -1:
+                    docno2 = did_res.loc[i, 'docno']
+                    if ids.loc[int(docno2), 'id'] == -1:
                         docnos.add(docno2)
-                        ids.loc[int(docno2)-1, 'id'] = id_count
+                        new_docs_count += 1
+                        ids.loc[int(docno2), 'id'] = id_count
                         dids.append(id_count)
                         id_count += 1
                     else:
-                        docnos.add(f'_{count}')
-                        dids.append(ids.iloc[int(docno2)-1]['id'])
-                        count += 1
-                        id_count += 1
-            # pad missing docids / edges
+                        dids.append(ids.loc[int(docno2), 'id'])
+            # pad missing docids
             if len(dids) < k:
-              print(dids)
               if did_res is None:
                 dids += [id_count] * (k - len(dids))
-                for i in range(k):
-                  docnos.add(f'_{count}')
-                  count += 1
               else:
                 dids += [id_count - len(did_res.index)] * (k - len(dids))
-                for i in range(k - len(did_res.index)):
-                  docnos.add(f'_{count}')
-                  count += 1
             # write neighbours
-            fe.write(np.array(dids, dtype=np.uint32).tobytes())
-    print(temp)
+            for _ in range(new_docs_count):
+                fe.write(np.array(dids, dtype=np.uint32).tobytes())
+    print(scored_count)
     # Finally, keep track of metadata about this artefact.
     with (out_dir/'pt_meta.json').open('wt') as fout:
       json.dump({
@@ -157,7 +151,6 @@ class NpTopKCorpusGraph(CorpusGraph):
       assert k <= self.meta['k']
     self._k = self.meta['k'] if k is None else k
     self._edges_path = self.path/'edges.u32.np'
-    self._weights_path = self.path/'weights.f16.np'
     self._docnos = Lookup(self.path/'docnos.npids')
 
   def __repr__(self):
@@ -172,16 +165,7 @@ class NpTopKCorpusGraph(CorpusGraph):
       res = res[:, :self._k]
     return res
 
-  @cached_property
-  def weights_data(self):
-    # loads data and alters to fit k
-    res = np.memmap(self._weights_path, mode='r',
-                    dtype=np.float16).reshape(-1, self._data_k)
-    if self._k != self._data_k:
-      res = res[:, :self._k]
-    return res
-
-  def neighbours(self, docid, weights=False):
+  def neighbours(self, docid):
     # returns neighbours using data
     as_str = isinstance(docid, str)
     if as_str:
@@ -190,9 +174,6 @@ class NpTopKCorpusGraph(CorpusGraph):
     neigh = self.edges_data[docid]
     if as_str:
       neigh = self._docnos.fwd[neigh]
-    if weights:
-      weigh = self.weights_data[docid]
-      return neigh, weigh
     return neigh
 
   def to_limit_k(self, k: int) -> 'NpTopKCorpusGraph':
