49af311bda82fc4f5d317c4e9b1d84a03692b691
LachlanJDunn <lachlan_dunn6@outlook.com>
2024-05-10 11:08:03 +1000

    1

--- corpus_graphs/corpus_algorithms/lexicographic.py
+++ corpus_graphs/corpus_algorithms/lexicographic.py
@@ -13,7 +13,7 @@ class LEXICOGRAPHIC(CORPUS_ALGORITHM):
     def __init__(self,
                  scorer: pt.Transformer,
                  corpus_graph: 'CorpusGraph',
-                 budget: int = 1000,
+                 budget: int = 0,
                  batch_size: Optional[int] = None,
                  verbose: bool = False,
                  collect_data: bool = False):
@@ -28,18 +28,18 @@ class LEXICOGRAPHIC(CORPUS_ALGORITHM):
         remaining = self.budget
         batch = batch.sort_values(by=['rank'])
         for did in batch.docno:
-            if remaining <= 0:
+            if remaining == 0:
                 break
             for target_did in self.corpus_graph.neighbours(did):
-                if remaining <= 0:
+                if remaining == 0:
                     break
                 if target_did not in to_score:
                     to_score[target_did] = 0
                     remaining -= 1
                     
         to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
-        to_score['qid'] = [qid for i in range(len(batch.index))]
-        to_score['query'] = [query for i in range(len(batch.index))]
+        to_score['qid'] = [qid for i in range(len(to_score))]
+        to_score['query'] = [query for i in range(len(to_score))]
 
         scored = self.scorer(to_score)
         self.scored_count += len(scored)
