faa88aa6cf7d5ba5f71bba4ae6821a7597456119
LachlanJDunn <lachlan_dunn6@outlook.com>
2024-06-12 17:19:38 +1000

    1

--- corpus_graphs/corpus_algorithms/scorer_spaces.py
+++ corpus_graphs/corpus_algorithms/scorer_spaces.py
@@ -5,6 +5,7 @@ from collections import Counter
 import pyterrier as pt
 import pandas as pd
 import ir_datasets
+import os
 import csv
 logger = ir_datasets.log.easy()
 
@@ -52,7 +53,7 @@ class SPACE():
             result['rank'].append(np.arange(min(len(scores), 1000)))
             for did, score in Counter(scores).most_common():
                 cutoff_count = 0
-                while cutoff_count < 1000:
+                if cutoff_count < 1000:
                     result['docno'].append(did)
                     result['score'].append(score)
                     cutoff_count += 1
@@ -70,6 +71,9 @@ class SPACE():
         for index, qid in enumerate(qids):
             locations.append(
                 {'in_location': doc_location_by_qid[qid][docnos[index]], 'out_location': np.concatenate(result['rank'])[index]})
+        newpath = f'{self.space_location}/{self.space_name}'
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
         with open(f'{self.space_location}/{self.space_name}/data.csv', 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=[
                                     'in_location', 'out_location'])
@@ -79,7 +83,7 @@ class SPACE():
             writer = csv.DictWriter(csvfile, fieldnames=['qid', 'query', 'docno', 'rank', 'score'])
             writer.writeheader()
             writer.writerows(final)
-        with open(f'{self.space_location}/{self.space_name}/metadata.txt', 'a') as file:
+        with open(f'{self.space_location}/{self.space_name}/metadata.txt', 'w') as file:
             file.write(f'{self.budget} {self.scored_count}\n')
         print('Total Documents Scored: ' + str(self.scored_count))
 
@@ -107,13 +111,12 @@ class SPACE1(SPACE):
             count = 1
             for target_did in self.corpus_graph.neighbours(did):
                 to_score[target_did] = 0
-                if self.collect_data:
-                    if target_did in self.doc_location:
-                        self.doc_location[target_did].append(
-                            (self.doc_location[did][0][0], count))
-                    else:
-                        self.doc_location[target_did] = [
-                            (self.doc_location[did][0][0], count)]
+                if target_did in self.doc_location:
+                    self.doc_location[target_did].append(
+                        (self.doc_location[did][0][0], count))
+                else:
+                    self.doc_location[target_did] = [
+                        (self.doc_location[did][0][0], count)]
                 count += 1
         to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
         to_score['qid'] = [qid for i in range(len(to_score))]
