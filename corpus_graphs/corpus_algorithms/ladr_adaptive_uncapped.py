52abfe2798a26f302db17f827fe6d0161b3b6e57
LachlanJDunn <lachlan_dunn6@outlook.com>
2024-06-15 14:59:43 +1000

    1

--- corpus_graphs/corpus_algorithms/ladr_adaptive_uncapped.py
+++ corpus_graphs/corpus_algorithms/ladr_adaptive_uncapped.py
@@ -9,25 +9,23 @@ import csv
 logger = ir_datasets.log.easy()
 
 
-class LADR_ADAPTIVE(CORPUS_ALGORITHM):
+class LADR_ADAPTIVE_UNCAPPED(CORPUS_ALGORITHM):
     def __init__(self,
                  scorer: pt.Transformer,
                  corpus_graph: 'CorpusGraph',
                  budget: int = 1000,
                  k: int = 1,
-                 c: int = 1,
                  batch_size: Optional[int] = None,
                  verbose: bool = False,
                  metadata: str = ''):
         super().__init__(scorer, corpus_graph, budget=budget,
                          batch_size=batch_size, verbose=verbose, metadata=metadata)
-        self.algorithm_type = 'ladr_adaptive'
+        self.algorithm_type = 'ladr_adaptive_uncapped'
         self.k = k
-        self.c = c
 
     def score_algorithm(self, batch, scores, qid, query):
         # Score initial documents
-        # Repeatedly score k neighbours of top c scored documents
+        # Repeatedly score k neighbours of top scored documents
         scored_list = []
         score_queue = {}
         scored_docs = {}
@@ -47,30 +45,27 @@ class LADR_ADAPTIVE(CORPUS_ALGORITHM):
 
         while remaining > 0:
             to_score = {}
-            for i in range(self.c):
-                if remaining <= 0:
+            did = max(score_queue, key=score_queue.get)
+            del score_queue[did]
+            k_count = 0
+            for target_did in self.corpus_graph.neighbours(did):
+                if remaining <= 0 or k_count >= self.k:
                     break
-                did = max(score_queue, key=score_queue.get)
-                del score_queue[did]
-                k_count = 0
-                for target_did in self.corpus_graph.neighbours(did):
-                    if remaining <= 0 or k_count >= self.k:
-                        break
-                    if target_did not in scored_docs and target_did not in to_score:
-                        to_score[target_did] = 0
-                        remaining -= 1
-                        k_count += 1
+                if target_did not in scored_docs and target_did not in to_score:
+                    to_score[target_did] = 0
+                    remaining -= 1
+                    k_count += 1
             if len(to_score.keys()) == 0:
-                break
+                pass
             to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
             to_score['qid'] = [qid for i in range(len(to_score))]
             to_score['query'] = [query for i in range(len(to_score))]
             batch_scored = self.scorer(to_score)
             scored_list.append(batch_scored)
-            score_queue.update(dict(zip(batch_scored.docno, batch_scored.score)))
+            score_queue.update(
+                dict(zip(batch_scored.docno, batch_scored.score)))
             scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
 
-        
         scored = pd.concat(scored_list)
         self.scored_count += len(scored)
         scores.update({k: s for k, s in zip(scored.docno, scored.score)})
