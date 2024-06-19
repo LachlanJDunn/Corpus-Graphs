2745f3d85e36b0da06e1c79670138f9569743cd9
LachlanJDunn <lachlan_dunn6@outlook.com>
2024-06-19 20:50:30 +1000

    1

--- corpus_graphs/corpus_algorithms/adaptive_threshold.py
+++ corpus_graphs/corpus_algorithms/adaptive_threshold.py
@@ -11,6 +11,92 @@ import math
 import statistics
 
 
+class ADAPTIVE_THRESHOLD(CORPUS_ALGORITHM):
+    def __init__(self,
+                 scorer: pt.Transformer,
+                 corpus_graph: 'CorpusGraph',
+                 budget: int = 1000,
+                 k: int = 1,
+                 threshold_sample_ratio: float = 0.1,
+                 batch_size: Optional[int] = None,
+                 verbose: bool = False,
+                 metadata: str = ''):
+        super().__init__(scorer, corpus_graph, budget=budget,
+                         batch_size=batch_size, verbose=verbose, metadata=metadata)
+        self.algorithm_type = 'adaptive_threshold'
+        self.k = k
+        self.threshold_sample_ratio = threshold_sample_ratio
+
+    def score_algorithm(self, batch, scores, qid, query):
+        # Use threshold_sample_ratio% budget to score initial retrieved documents (establishes threshold)
+        # Alternate: score initial retrieved document, perform ladr_adaptive on neighbours of initial retrieved document until exhaustion (expanding only if document passes threshold)
+
+        scored_list = []
+        score_queue = {}
+        batch_queue = {}
+        scored_docs = {}
+        to_score = {}
+
+        to_score.update({k: 0 for k in batch.docno[:math.floor(
+            self.budget * self.threshold_sample_ratio)]})
+        to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
+        to_score['qid'] = [qid for i in range(len(to_score))]
+        to_score['query'] = [query for i in range(len(to_score))]
+        batch_scored = self.scorer(to_score)
+        threshold = self.calculate_threshold(batch_scored)
+        scored_list.append(batch_scored)
+        score_queue.update(dict(zip(batch_scored.docno, batch_scored.score)))
+        scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
+
+        batch_queue.update(dict(zip(batch.docno, batch.score)))
+
+        remaining = self.budget - len(to_score.docno)
+
+        while remaining > 0:
+            to_score = {}
+            if len(score_queue) > 0:
+                did = max(score_queue, key=score_queue.get)
+                del score_queue[did]
+                k_count = 0
+                for target_did in self.corpus_graph.neighbours(did):
+                    if remaining <= 0 or k_count >= self.k:
+                        break
+                    if target_did not in scored_docs and target_did not in to_score:
+                        to_score[target_did] = 0
+                        remaining -= 1
+                        k_count += 1
+                if len(to_score.keys()) > 0:
+                    to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
+                    to_score['qid'] = [qid for i in range(len(to_score))]
+                    to_score['query'] = [query for i in range(len(to_score))]
+                    batch_scored = self.scorer(to_score)
+                    scored_list.append(batch_scored)
+                    score_queue.update(dict(
+                        [x for x in zip(batch_scored.docno, batch_scored.score) if x[1] >= threshold]))
+                    scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
+            else:
+                if len(batch_queue) == 0:
+                    break
+                did = max(batch_queue, key=batch_queue.get)
+                del batch_queue[did]
+                if did not in scored_docs:
+                    to_score = pd.DataFrame([did], columns=['docno'])
+                    to_score['qid'] = [qid]
+                    to_score['query'] = [query]
+                    batch_scored = self.scorer(to_score)
+                    scored_list.append(batch_scored)
+                    score_queue.update(
+                        dict(zip(batch_scored.docno, batch_scored.score)))
+                    scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
+
+        scored = pd.concat(scored_list)
+        self.scored_count += len(scored)
+        scores.update({k: s for k, s in zip(scored.docno, scored.score)})
+
+    def calculate_threshold(self, batch):
+        return 0
+
+
 '''class ADAPTIVE_THRESHOLD(CORPUS_ALGORITHM):
     def __init__(self,
                  scorer: pt.Transformer,
@@ -149,87 +235,3 @@ class ADAPTIVE_THRESHOLD_SD(ADAPTIVE_THRESHOLD):
         return mean + self.sd * sd
 
 
-class ADAPTIVE_THRESHOLD(CORPUS_ALGORITHM):
-    def __init__(self,
-                 scorer: pt.Transformer,
-                 corpus_graph: 'CorpusGraph',
-                 budget: int = 1000,
-                 k: int = 1,
-                 threshold_sample_ratio: float = 0.1,
-                 batch_size: Optional[int] = None,
-                 verbose: bool = False,
-                 metadata: str = ''):
-        super().__init__(scorer, corpus_graph, budget=budget,
-                         batch_size=batch_size, verbose=verbose, metadata=metadata)
-        self.algorithm_type = 'adaptive_threshold'
-        self.k = k
-        self.threshold_sample_ratio = threshold_sample_ratio
-
-    def score_algorithm(self, batch, scores, qid, query):
-        # Use threshold_sample_ratio% budget to score initial retrieved documents (establishes threshold)
-        # Alternate: score initial retrieved document, perform ladr_adaptive on neighbours of initial retrieved document until exhaustion (expanding only if document passes threshold)
-
-        scored_list = []
-        score_queue = {}
-        batch_queue = {}
-        scored_docs = {}
-        to_score = {}
-
-        to_score.update({k: 0 for k in batch.docno[:math.floor(
-            self.budget * self.threshold_sample_ratio)]})
-        to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
-        to_score['qid'] = [qid for i in range(len(to_score))]
-        to_score['query'] = [query for i in range(len(to_score))]
-        batch_scored = self.scorer(to_score)
-        threshold = self.calculate_threshold(batch_scored)
-        scored_list.append(batch_scored)
-        score_queue.update(dict(zip(batch_scored.docno, batch_scored.score)))
-        scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
-
-        batch_queue.update(dict(zip(batch.docno, batch.score)))
-
-        remaining = self.budget - len(to_score.docno)
-
-        while remaining > 0:
-            to_score = {}
-            if len(score_queue) > 0:
-                did = max(score_queue, key=score_queue.get)
-                del score_queue[did]
-                k_count = 0
-                for target_did in self.corpus_graph.neighbours(did):
-                    if remaining <= 0 or k_count >= self.k:
-                        break
-                    if target_did not in scored_docs and target_did not in to_score:
-                        to_score[target_did] = 0
-                        remaining -= 1
-                        k_count += 1
-                if len(to_score.keys()) > 0:
-                    to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
-                    to_score['qid'] = [qid for i in range(len(to_score))]
-                    to_score['query'] = [query for i in range(len(to_score))]
-                    batch_scored = self.scorer(to_score)
-                    scored_list.append(batch_scored)
-                    score_queue.update(dict(
-                        [x for x in zip(batch_scored.docno, batch_scored.score) if x[1] >= threshold]))
-                    scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
-            else:
-                if len(batch_queue) == 0:
-                    break
-                did = max(batch_queue, key=batch_queue.get)
-                del batch_queue[did]
-                if did not in scored_docs:
-                    to_score = pd.DataFrame([did], columns=['docno'])
-                    to_score['qid'] = [qid]
-                    to_score['query'] = [query]
-                    batch_scored = self.scorer(to_score)
-                    scored_list.append(batch_scored)
-                    score_queue.update(
-                        dict(zip(batch_scored.docno, batch_scored.score)))
-                    scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
-
-        scored = pd.concat(scored_list)
-        self.scored_count += len(scored)
-        scores.update({k: s for k, s in zip(scored.docno, scored.score)})
-
-    def calculate_threshold(self, batch):
-        return 0
