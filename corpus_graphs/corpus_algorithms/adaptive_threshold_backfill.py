cc2576e880a651e87c9d775c834fa8781fb5dc06
LachlanJDunn <lachlan_dunn6@outlook.com>
2024-06-20 21:01:47 +1000

    1

--- corpus_graphs/corpus_algorithms/adaptive_threshold_backfill.py
+++ corpus_graphs/corpus_algorithms/adaptive_threshold_backfill.py
@@ -11,7 +11,7 @@ import csv
 logger = ir_datasets.log.easy()
 
 
-class ADAPTIVE_THRESHOLD_BACKFILL(CORPUS_ALGORITHM):
+'''class ADAPTIVE_THRESHOLD_BACKFILL(CORPUS_ALGORITHM):
     def __init__(self,
                  scorer: pt.Transformer,
                  corpus_graph: 'CorpusGraph',
@@ -22,7 +22,7 @@ class ADAPTIVE_THRESHOLD_BACKFILL(CORPUS_ALGORITHM):
                  initial_scored_min: int = 0):
         super().__init__(scorer, corpus_graph, budget=budget,
                          verbose=verbose, metadata=metadata)
-        self.algorithm_type = 'adaptive_threshold'
+        self.algorithm_type = 'adaptive_threshold_backfill'
         self.initial_scored_min = initial_scored_min
         self.k = k
 
@@ -107,6 +107,105 @@ class ADAPTIVE_THRESHOLD_BACKFILL(CORPUS_ALGORITHM):
         scores.update({k: s for k, s in zip(scored.docno, scored.score)})
 
     def calculate_threshold(self, batch, d):
+        return 0'''
+
+
+class ADAPTIVE_THRESHOLD_BACKFILL(CORPUS_ALGORITHM):
+    def __init__(self,
+                 scorer: pt.Transformer,
+                 corpus_graph: 'CorpusGraph',
+                 budget: int = 1000,
+                 k: int = 1,
+                 verbose: bool = False,
+                 metadata: str = '',
+                 initial_scored_min: int = 0):
+        super().__init__(scorer, corpus_graph, budget=budget,
+                         verbose=verbose, metadata=metadata)
+        self.algorithm_type = 'adaptive_threshold_backfill'
+        self.initial_scored_min = initial_scored_min
+        self.k = k
+
+    def score_algorithm(self, batch, scores, qid, query):
+        # Score top d initial retrieved documents (establishes threshold)
+        # Alternate: score initial retrieved document, perform ladr_adaptive on neighbours of initial retrieved document until exhaustion (expanding only if document passes threshold)
+        # Scores at least initial_score_min from initial retrieved documents (unless backfilling results in >= initial_score_min documents being added)
+        d = 50
+        scored_list = []
+        score_queue = {}
+        batch_queue = {}
+        scored_docs = {}
+        to_score = {}
+
+        to_score.update({k: 0 for k in batch.docno[:d]})
+        to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
+        to_score['qid'] = [qid for i in range(len(to_score))]
+        to_score['query'] = [query for i in range(len(to_score))]
+        batch_scored = self.scorer(to_score)
+        threshold = self.calculate_threshold(batch_scored, d)
+        scored_list.append(batch_scored)
+        score_queue.update(dict(zip(batch_scored.docno, batch_scored.score)))
+        scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
+
+        batch_queue.update(dict(zip(batch.docno, batch.score)))
+
+        remaining = self.budget - len(to_score.docno)
+
+        while remaining > 0:
+            to_score = {}
+            if remaining <= self.budget/3 and len(batch_queue) > 0:
+                did = max(batch_queue, key=batch_queue.get)
+                del batch_queue[did]
+                if did not in scored_docs:
+                    to_score = pd.DataFrame([did], columns=['docno'])
+                    to_score['qid'] = [qid]
+                    to_score['query'] = [query]
+                    batch_scored = self.scorer(to_score)
+                    remaining -= 1
+                    scored_list.append(batch_scored)
+                    score_queue.update(
+                        dict(zip(batch_scored.docno, batch_scored.score)))
+                    scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
+            elif len(score_queue) > 0:
+                did = max(score_queue, key=score_queue.get)
+                del score_queue[did]
+                k_count = 0
+                for target_did in self.corpus_graph.neighbours(did):
+                    if remaining <= 0 or k_count >= self.k:
+                        break
+                    if target_did not in scored_docs and target_did not in to_score:
+                        to_score[target_did] = 0
+                        remaining -= 1
+                        k_count += 1
+                if len(to_score.keys()) > 0:
+                    to_score = pd.DataFrame(to_score.keys(), columns=['docno'])
+                    to_score['qid'] = [qid for i in range(len(to_score))]
+                    to_score['query'] = [query for i in range(len(to_score))]
+                    batch_scored = self.scorer(to_score)
+                    scored_list.append(batch_scored)
+                    score_queue.update(dict(
+                        [x for x in zip(batch_scored.docno, batch_scored.score) if x[1] >= threshold]))
+                    scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
+            else:
+                if len(batch_queue) == 0:
+                    break
+                did = max(batch_queue, key=batch_queue.get)
+                del batch_queue[did]
+                if did not in scored_docs:
+                    to_score = pd.DataFrame([did], columns=['docno'])
+                    to_score['qid'] = [qid]
+                    to_score['query'] = [query]
+                    batch_scored = self.scorer(to_score)
+                    remaining -= 1
+                    scored_list.append(batch_scored)
+                    score_queue.update(
+                        dict(zip(batch_scored.docno, batch_scored.score)))
+                    scored_docs.update(dict.fromkeys(batch_scored.docno, 0))
+
+        scored = pd.concat(scored_list)
+        self.scored_count += len(scored)
+        scores.update({k: s for k, s in zip(scored.docno, scored.score)})
+
+    def calculate_threshold(self, batch, d):
         return 0
 
 
@@ -121,7 +220,7 @@ class ADAPTIVE_THRESHOLD_BACKFILL_MEAN(ADAPTIVE_THRESHOLD_BACKFILL):
                  initial_scored_min: int = 0):
         super().__init__(scorer, corpus_graph, budget=budget, k=k,
                          verbose=verbose, metadata=metadata, initial_scored_min=initial_scored_min)
-        self.algorithm_type = 'adaptive_threshold_mean'
+        self.algorithm_type = 'adaptive_threshold_backfill_mean'
 
     def calculate_threshold(self, batch, d):
         return statistics.mean(batch.score[:d])
